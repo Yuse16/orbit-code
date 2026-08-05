6d8a188d655492ca372b9edfd76c24c9f4ef5652
Jorge De La Peña <volcolm_jorge@hotmail.com>
2026-08-05 13:57:06 -0600

    chore: prepare static Next frontend for Tauri

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -35,10 +34,7 @@ export default function RootLayout({
       lang="es"
       className={`dark bg-background ${geistSans.variable} ${geistMono.variable}`}
     >
-      <body className="font-sans antialiased">
-        {children}
-        {process.env.NODE_ENV === 'production' && <Analytics />}
-      </body>
+      <body className="font-sans antialiased">{children}</body>
     </html>
   )
 }
